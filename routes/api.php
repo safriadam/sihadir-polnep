3691c423b424743f07bff9b0640a48441d78297c
Amelia8855 <ameliaiyuk@gamil.com>
2024-08-01 11:38:22 +0700

    create add spController

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SpController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/Dashboard-sp',[spController::class, 'Dashboardsp']); 
+// (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
