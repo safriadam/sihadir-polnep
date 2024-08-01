fd01417d4d6995b9f663cde25461d7d0097c685a
unknown <ideliafatihaaf@gmail.com>
2024-08-01 11:30:37 +0700

    create controller cicil kompen

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CicilKompenController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/Dashboard-cicil-kompen',[CicilKompenController::class, 'DashboardCicil']); 
+
+// function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
